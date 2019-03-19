3353fa8079cdf9381b355eb03a90cb092b2f3518
Bartolomeu Rodrigues <pbartmr@gmail.com>
2019-03-19 14:11:09 +0000

    finished step-03

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/shared/nav-bar/nav-bar';
 import LoginPage from './components/pages/login/login';
+import { AppContext, appInitialState } from './app-context';
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...appInitialState
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <NavBar></NavBar>
-        <LoginPage></LoginPage>
-      </div>
+      <AppContext.Provider value={this.state}>
+        <div className="App">
+          <NavBar></NavBar>
+          <LoginPage></LoginPage>
+        </div>
+      </AppContext.Provider>
     );
   }
 }
